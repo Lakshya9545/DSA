667dcea3859d44fe876440684659b11af38e2bcd
Lakshya9545 <pandyalakshya04@gmail.com>
2023-10-20 00:42:02 +0530

    updated stack using array

--- Stack/stack_in_array.c
+++ Stack/stack_in_array.c
@@ -1,38 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX 100 // Define MAX as a constant (adjust the size as needed)
-int stack_arr[MAX];
-int top = -1;
+#define max 5
 
-void push(int data)
+int top = -1;
+int stack[max];
+
+void push(int x)
 {
-    if (top == MAX - 1)
+    if (top == max - 1)
     {
-        printf("Stack is full\n");
+        printf("Stack overflow\n");
         return;
     }
-    top++;
-    stack_arr[top] = data;
+    stack[++top] = x;
+}
+
+int pop()
+{
+    if (top == -1)
+    {
+        printf("Stack underflow\n");
+        return -1;
+    }
+    return stack[top--];
+}
+
+void display()
+{
+    int i;
+    if (top == -1)
+    {
+        printf("Stack is empty\n");
+        return;
+    }
+    printf("Stack elements: ");
+    for (i = top; i >= 0; i--)
+    {
+        printf("%d ", stack[i]);
+    }
+    printf("\n");
+}
+
+void peek()
+{
+    if (top == -1)
+    {
+        printf("Stack is empty\n");
+        return;
+    }
+    printf("Top element: %d\n", stack[top]);
 }
 
 int main()
 {
-    int data;
-    printf("Enter the elements to push into the stack: ");
-    for (int i = 0; i < MAX; i++)
+    int ch, data;
+    while (1)
     {
-        printf("Enter element %d: ", i + 1);
-        scanf("%d", &data);
-        push(data);
-    }
+        printf("\n1. Push\n2. Pop\n3. Display\n4. Peek\n5. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &ch);
 
-    printf("Stack contents: ");
-    for (int i = top; i >= 0; i--)
-    {
-        printf("%d ", stack_arr[i]);
+        switch (ch)
+        {
+        case 1:
+            printf("Enter data to be inserted: ");
+            scanf("%d", &data);
+            push(data);
+            break;
+        case 2:
+            data = pop();
+            if (data != -1)
+            {
+                printf("Popped element: %d\n", data);
+            }
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            peek();
+            break;
+        case 5:
+            exit(0);
+        default:
+            printf("Invalid choice. Please enter a valid option.\n");
+        }
     }
-    printf("\n");
 
     return 0;
 }
